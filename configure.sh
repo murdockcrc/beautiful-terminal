fe62f1c18b000f95d449b26a82939154a72b0bb7
Luis Delgado <luis@delgado.ms>
2022-12-20 10:48:04 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -21,7 +21,7 @@ rm packages-microsoft-prod.deb
 sudo apt-get update; \
   sudo apt-get install -y apt-transport-https && \
   sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-7.0
+  sudo apt-get install -y dotnet-sdk-6.0
 
 # zsh install
 which zsh > /dev/null 2>&1
