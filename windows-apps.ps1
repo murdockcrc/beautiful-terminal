860f43244df2f77f4c21addaca471873aa6e4dce
Luis Delgado <murdockcrc@icloud.com>
2024-02-16 09:47:20 +0100

    Adds new windows tools

--- windows-apps.ps1
+++ windows-apps.ps1
@@ -1,2 +1,8 @@
 winget install -e -h --id Microsoft.VisualStudioCode
 winget install -e -h --id Microsoft.PowerToys
+winget install -e -h --id Git.Git
+winget install -e -h --id Docker.DockerDesktop
+winget install -e -h --id Microsoft.VisualStudio.2022.Enterprise
+winget install -e -h --id Microsoft.SQLServerManagementStudio
+winget install -e -h --id Microsoft.Azure.StorageExplorer
+winget install -e -h --id Microsoft.AzureCLI
