4e80ef670db3ff242a71a14c611a0ec52e24f587
Luis Delgado <murdockcrc@icloud.com>
2024-08-20 14:27:50 +0200

    Update windows-apps.ps1

--- windows-apps.ps1
+++ windows-apps.ps1
@@ -1,5 +1,4 @@
 winget install -e -h --id Microsoft.VisualStudioCode
-winget install -e -h --id Microsoft.PowerToys
 winget install -e -h --id Git.Git
 winget install -e -h --id Docker.DockerDesktop
 winget install -e -h --id Microsoft.VisualStudio.2022.Enterprise
